36342d5746873528799a9a5804fe743d00db4625
Jennie <jentdev@gmail.com>
2023-09-04 19:14:50 -0700

    added js function to extract data from form

---
+++ assets/js/form.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const fd = new FormData(form);
+    const obj = Object.fromEntries(fd);
+
+    const json = JSON.stringify(obj);
+    localStorage.setItem('form', json);
+
+    window.location.href = 'confirmation.html';
+});
